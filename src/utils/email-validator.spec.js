92376c5acc5a22d68f97c59519155eae07565de8
juuliana <jumaju10@hotmail.com>
2022-03-26 17:00:02 -0300

    feat: create test with returns true or false -> validator

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,14 +1,24 @@
+const validator = require('validator')
+
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
     const sut = new EmailValidator()
-    const isEmailValid = sut.isValid('valid_email@mail.com')
+    const isEmailValid = sut.isValid('valid_email@email.com')
 
     expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email@email.com')
+
+    expect(isEmailValid).toBe(false)
   })
 })
