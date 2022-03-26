c2c3fd1254556882cd8361e6689db0d351fc54ca
juuliana <jumaju10@hotmail.com>
2022-03-26 17:01:36 -0300

    feat: create makeSut()

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -6,9 +6,13 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => {
+  return new EmailValidator()
+};
+
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@email.com')
 
     expect(isEmailValid).toBe(true)
@@ -16,7 +20,7 @@ describe('Email Validator', () => {
 
   test('Should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email@email.com')
 
     expect(isEmailValid).toBe(false)
