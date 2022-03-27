ffc5fd9a6238a9d2693865b39f7c90effe302858
juuliana <jumaju10@hotmail.com>
2022-03-27 15:01:32 -0300

    feat: ensure Token Generator call jwt with correct values

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,15 +1,19 @@
 const jwt = require('jsonwebtoken')
 
 class TokenGenerator {
+  constructor (secretKey) {
+    this.secretKey = secretKey
+  }
+
   async generate (id) {
-    const token = jwt.sign(id, 'secret_key')
+    const token = jwt.sign(id, this.secretKey)
 
     return token
   }
 }
 
 const makeSut = () => {
-  const sut = new TokenGenerator()
+  const sut = new TokenGenerator('secret_key')
 
   return { sut }
 };
@@ -26,5 +30,12 @@ describe('Token Generator', () => {
     const { sut } = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
+  })
+
+  test('Should call jwt with correct values', async () => {
+    const { sut } = makeSut()
+    await sut.generate('any_id')
+    expect(jwt.id).toBe('any_id')
+    expect(jwt.secretKey).toBe(sut.secretKey)
   })
 })
