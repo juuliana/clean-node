a7c23d60f6dfd45867a4a8556094d47b6f654421
juuliana <jumaju10@hotmail.com>
2022-03-27 14:33:19 -0300

    feat: move Encrypter for own file

---
+++ src/utils/helpers/encrypter.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypt')
+
+module.exports = class Encrypter {
+  async compare (value, hash) {
+    const isValid = await bcrypt.compare(value, hash)
+
+    return isValid
+  }
+}
