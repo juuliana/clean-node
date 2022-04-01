159dbebdcfd8c3212a1cd02401bd856f612eaaa5
juuliana <jumaju10@hotmail.com>
2022-03-31 21:22:35 -0300

    feat: ensure throw if no email is provided

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,5 +1,6 @@
 const MongoHelper = require('../helpers/mongo-helper')
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
+const MissingParamError = require('../../utils/errors/missing-param-error')
 
 let db
 
@@ -47,5 +48,12 @@ describe('LoadUserByEmail Repository', () => {
     const promise = sut.load('any_email@email.com')
 
     expect(promise).rejects.toThrow()
+  })
+
+  test('Should throw if no email is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.load()
+
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
