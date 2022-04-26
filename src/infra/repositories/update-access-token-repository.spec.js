b15ac393f695fb68f3ea1f47a6561825676a0f0d
juuliana <jumaju10@hotmail.com>
2022-04-26 19:58:09 -0300

    feat: refactor with makeSut

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -21,6 +21,14 @@ class UpdateAccessTokenRepository {
   }
 }
 
+const makeSut = () => {
+  const userModel = db.collection("users");
+
+  const sut = new UpdateAccessTokenRepository(userModel);
+
+  return { userModel, sut };
+};
+
 describe("UpdateAccessToken Repository ", () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
@@ -36,9 +44,7 @@ describe("UpdateAccessToken Repository ", () => {
   });
 
   it("Should update the user with the given accessToken", async () => {
-    const userModel = db.collection("users");
-
-    const sut = new UpdateAccessTokenRepository(userModel);
+    const { sut, userModel } = makeSut();
 
     const fakeUser = await userModel.insertOne({
       email: "valid_email@email.com",
