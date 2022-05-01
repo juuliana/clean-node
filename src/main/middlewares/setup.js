3d99612cc15e379ded006be23cd99e18161f922b
juuliana <jumaju10@hotmail.com>
2022-04-30 22:56:28 -0300

    feat: add content_type

--- src/main/middlewares/setup.js
+++ src/main/middlewares/setup.js
@@ -1,10 +1,8 @@
 const cors = require('../middlewares/cors')
 const jsonParser = require('../middlewares/json-parser')
-const contentType = require('../middlewares/content-type')
 
 module.exports = (app) => {
   app.disable('x-powered-by')
   app.use(cors)
   app.use(jsonParser)
-  app.use(contentType)
 };
