b8445872c7bcec84cab27d09d6e3cdbc507ef8ca
juuliana <jumaju10@hotmail.com>
2022-04-30 22:34:31 -0300

    feat: enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -1,14 +1,26 @@
-const request = require("supertest");
-const app = require("./app");
+const request = require('supertest')
+const app = require('./app')
 
-describe("App Setup ", () => {
-  test("Should disable x-powered-by header", async () => {
-    app.get("/test_x_powered_by", (req, res) => {
-      res.send("");
-    });
+describe('App Setup ', () => {
+  test('Should disable x-powered-by header', async () => {
+    app.get('/test_x_powered_by', (req, res) => {
+      res.send('')
+    })
 
-    const res = await request(app).get("/");
+    const res = await request(app).get('/')
 
-    expect(res.headers["x-powered-by"]).toBe(undefined);
-  });
-});
+    expect(res.headers['x-powered-by']).toBe(undefined)
+  })
+
+  test('Should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_cors')
+
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
+})
