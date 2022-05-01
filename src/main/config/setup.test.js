87f94293301bfbbc43f0967bb30a9c87a7437713
juuliana <jumaju10@hotmail.com>
2022-04-30 22:39:22 -0300

    fix: move test cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -11,16 +11,4 @@ describe('App Setup ', () => {
 
     expect(res.headers['x-powered-by']).toBe(undefined)
   })
-
-  test('Should enable cors', async () => {
-    app.get('/test_cors', (req, res) => {
-      res.send('')
-    })
-
-    const res = await request(app).get('/test_cors')
-
-    expect(res.headers['access-control-allow-origin']).toBe('*')
-    expect(res.headers['access-control-allow-methods']).toBe('*')
-    expect(res.headers['access-control-allow-headers']).toBe('*')
-  })
 })
