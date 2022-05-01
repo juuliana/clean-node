9e16da549d7ce60ecee8dbad1bc16ebab56a66eb
juuliana <jumaju10@hotmail.com>
2022-05-01 00:01:49 -0300

    fix: move email validator

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,7 +1,7 @@
 const validator = require('validator')
 
 const EmailValidator = require('./email-validator')
-const MissingParamError = require('./errors/missing-param-error')
+const MissingParamError = require('../errors/missing-param-error')
 
 const makeSut = () => {
   return new EmailValidator()
